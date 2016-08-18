371faded97200f85359194f8068565e594cd1df5
haxwithaxe <me@haxwithaxe.net>
2016-08-17 23:54:57 -0400

    fancy setup.py that uses git for the version

--- setup.py
+++ setup.py
@@ -1,12 +1,37 @@
 from distutils.core import setup
+import os
+import subprocess
 
-from passwordcard import __version__
 
+VERSION_FILE = 'VERSION'
 NAME = 'passwordcard'
 MODULES = [NAME]
 
+
+def get_version():
+    clean = ''
+    if os.path.exists('.git'):
+        clean_stdout = subprocess.check_output(['git', 'status', '--porcelain'], stderr=subprocess.DEVNULL)
+        if len(clean_stdout.split()) > 0:
+            clean = '-unclean'
+        try:
+            tags = subprocess.check_output(['git', 'describe', '--tags'], stderr=subprocess.DEVNULL)
+            stdout, stderr = describe.interact()
+            version = '%s%s' % (stdout.split('\n')[0].decode().strip(), clean)
+        except (subprocess.CalledProcessError, IndexError):
+            version = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD'])
+            version = '0.0-%s%s' % (version.decode().strip(), clean)
+        with open(VERSION_FILE, 'w') as version_file:
+            version += '\n'
+            version_file.write(version)
+    else:
+        with open(VERSION_FILE, 'r') as version_file:
+            version = version_file.readline().strip().decode()
+    return version
+
+
 setup(
 	name=NAME,
-	version=__version__,
+	version=get_version(),
 	packages=MODULES
 	)
